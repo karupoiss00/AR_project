fddd2e82ab09d570b02b6476d712db6348146460
karupoiss00 <glizerin1108@gmail.com>
2019-07-30 13:44:34 +0300

    Тестовый код

--- js/index.js
+++ js/index.js
@@ -165,8 +165,8 @@ function update()
 
         for (const mesh of tipMeshes)
         {
-            mesh.getWorldScale(worldScale);
-            scales.concat(worldScale.x.toString(), " ", worldScale.y.toString(), worldScale.z.toString(), " | ");
+            mesh.getWorldPosition (worldScale);
+            scales.concat(worldScale.x, " ", worldScale.y, worldScale.z, " | ");
         }
         console.log(scales);
     }
