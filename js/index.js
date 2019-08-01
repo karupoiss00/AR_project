730bbf88e47b78634a687e727a823b0a856054e1
karupoiss00 <glizerin1108@gmail.com>
2019-08-01 12:06:22 +0300

    Доработка фикса расстояния

--- js/index.js
+++ js/index.js
@@ -167,7 +167,7 @@ function showTips() {
         mesh.visible = false;
     }
     const [tipMesh, dist] = getNearestTip();
-    if (dist < 15)
+    if (dist < 18)
     {
         tipMesh.visible = true;
     }
