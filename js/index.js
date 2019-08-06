f8fe83f5066158a472880d0aaae275071e8c915e
karupoiss00 <glizerin1108@gmail.com>
2019-08-06 12:22:04 +0300

    Фикс отрисовки подсказок

--- js/index.js
+++ js/index.js
@@ -158,6 +158,7 @@ function loadTips(marker, url) {
             console.log(tip);
             const tipMesh = createTipMesh(tip);
             tipMeshes.push(tipMesh);
+            tipMesh.visible = true;
             marker.add(tipMesh);
         }
         console.log(tipMeshes);
@@ -194,7 +195,6 @@ function showTips() {
     {
         mesh.visible = false;
     }
-    tipMeshes[4].visible = true;
 	const {tipMesh, distance} = getNearestTip();
 
     if (distance < 25)
