af0e693881de76c65ead3a9a378b5018ae84c667
karupoiss00 <glizerin1108@gmail.com>
2019-08-07 10:15:02 +0300

    Фикс загрузки подсказок из json

--- js/index.js
+++ js/index.js
@@ -146,7 +146,7 @@ function loadModel(marker, path, mtlName, objName, scale) {
  * @param {string} url
  */
 function loadTips(marker, url) {
-
+    /*
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.onload = () => {
@@ -155,21 +155,21 @@ function loadTips(marker, url) {
         {
             tipsData.push(tip);
         }
-        for (const tip of tipsData)
-        {
-            console.log(tip);
-            const tipMesh = createTipMesh(tip);
-            tipMeshes.push(tipMesh);
-            marker.add(tipMesh);
-        }
+
         console.log(tipMeshes);
     };
     xhr.onerror = () => {
         console.log("Failed to load tips.json");
     };
 
-    xhr.send();
-
+    xhr.send();*/
+    for (const tip of tipsData)
+    {
+        console.log(JSON.stringify(tip));
+        const tipMesh = createTipMesh(tip);
+        tipMeshes.push(tipMesh);
+        marker.add(tipMesh);
+    }
 
 }
 
