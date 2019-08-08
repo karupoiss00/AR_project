bdd7c4292de59d7b7d1d78afe9c732cc81bfe740
karupoiss00 <glizerin1108@gmail.com>
2019-08-08 12:02:58 +0300

    Фикс php-скрипта

--- php/saveFile.php
+++ php/saveFile.php
@@ -3,11 +3,9 @@
     $filename = urldecode($_POST["filename"]);
     $content = urldecode($_POST["content"]);
 
-    mkdir('/saves/'.$dir, 0700);
-
-    copy('./index.html', 'saves/'.$dir.'/index.html');
-    copy('./index.js', 'saves/'.$dir.'/index.js');
-    $file = fopen('saves/'.$dir.'/'.$filename, 'w') or die("Can't open file");
+    copy('./index.html', 'saves/test/index.html');
+    copy('./index.js', 'saves/test/index.js');
+    $file = fopen('saves/test/'.$filename, 'w') or die("Can't open file");
     fwrite($file, $content);
     fclose($file);
 ?>
