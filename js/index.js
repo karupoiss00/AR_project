152860ea81c04a6b90faefee5b1d786c1fac89d3
karupoiss00 <glizerin1108@gmail.com>
2019-08-09 10:23:44 +0300

    Фикс saveTips

--- js/index.js
+++ js/index.js
@@ -165,8 +165,7 @@ function saveTips(tipsData) {
     const workId = randomId();
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://karupoiss00.000webhostapp.com/saveWork.php', true); // Открываем асинхронное соединение
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); // Отправляем кодировку
-    xhr.setRequestHeader('CAccess-Control-Allow-Origin', '*');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onload = () => {
         document.body.innerHTML += 'Your work id: ' + workId;
     };
