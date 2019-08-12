c57efb784a7924df0614790065a4f0d9ddfc4d37
karupoiss00 <glizerin1108@gmail.com>
2019-08-12 10:24:12 +0300

    Фикс кнопки back

--- js/index.js
+++ js/index.js
@@ -321,7 +321,7 @@ function addTip() {
  */
 function back(hasCamera)
 {
-    if (arToolkitSource != null) {
+    if (arToolkitSource != undefined) {
         arToolkitSource.domElement.style.visibility = "hidden";
     }
     renderer.domElement.style.visibility = "hidden";
