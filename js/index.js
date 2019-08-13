6dd81cce9df7a713593d325e29e128c2deb08a7c
karupoiss00 <glizerin1108@gmail.com>
2019-08-13 12:16:20 +0300

    Лог markerRoot

--- js/index.js
+++ js/index.js
@@ -234,6 +234,12 @@ function onResize(hasCamera) {
 
 }
 
+function updateGroupPosition() {
+    const groupPos = new THREE.Vector3(markerRoot.position.x, markerRoot.position.y, tipMeshes[i].position.z);
+    const distance = THREE.Vector3(0, 0, 0).distanceToSquared(groupPos);
+    console.log(distance);
+}
+
 /**
  * @param {boolean} hasCamera
  */
