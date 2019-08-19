c122f41162238be9e80952aea4826fd162c953f6
karupoiss00 <glizerin1108@gmail.com>
2019-08-19 09:23:32 +0300

    Попытка восстановления позиционирования

--- js/index.js
+++ js/index.js
@@ -394,7 +394,7 @@ window.onload = function() {
             rotationMatrix[13] = markerRoot.getWorldPosition().y;
             rotationMatrix[14] = markerRoot.getWorldPosition().z;
             markerRoot.matrix.fromArray(rotationMatrix);
-            rotateGroup(0, rotation.y, rotation.z)
+            rotateGroup(rotation.x, rotation.y, rotation.z)
         }
     }
 
