bc29e68e953a4640d46686da72ba62866f6621bc
karupoiss00 <glizerin1108@gmail.com>
2019-08-20 12:36:56 +0300

    Фикс слайдера

--- js/index.js
+++ js/index.js
@@ -264,7 +264,7 @@ function update(hasCamera) {
     if (isFixed)
     {
         showElement('rotator');
-        markerRoots[0].rotation.y += THREE.Math.degToRad(getNumberValue('rotator'));
+        markerRoots[0].rotation.y = THREE.Math.degToRad(getNumberValue('rotator'));
     }
     else
     {
